22638644168904edcf94e670d07f6c3c33f09f52
Michael Wiseman <mcwiseman97@gmail.com>
2021-02-03 12:58:01 -0800

    Test functions

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-  cout << "Hello World!\n";
-  cout << "Bye World!\n";
-  return 0;
+// function declaration
+int max(int num1, int num2);
+ 
+int main () {
+   // local variable declaration:
+   int a = 100;
+   int b = 200;
+   int ret;
+ 
+   // calling a function to get max value.
+   ret = max(a, b);
+   cout << "Max value is : " << ret << endl;
+ 
+   return 0;
+}
+ 
+// function returning the max between two numbers
+int max(int num1, int num2) {
+   // local variable declaration
+   int result;
+
+    cout << "Test\n";
+ 
+   if (num1 > num2)
+      result = num1;
+   else
+      result = num2;
+ 
+   return result; 
 }
